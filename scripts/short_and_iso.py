957956e380954429fef03258e314a987fcdd6ce0
Mathieu Renzo <mrenzo@arizona.edu>
2025-03-20 06:27:40 -0700

    scripts to convert MESA profiles to short and isotopes data for SNEC

--- scripts/short_and_iso.py
+++ scripts/short_and_iso.py
@@ -4,11 +4,11 @@ import os
 import sys
 import subprocess
 
+# Edit line below to point to the location
+where_the_scripts_are = "/home/mrenzo/Runs/TARDIS_connector_workshop/SESN_progenitor/scripts/"
+
 profile = sys.argv[1]
 
-os.system('python ./mesa_to_GR1D.py '+profile)
-#rename the short file
-rename_command='mv *.short '+profile+'.short'
-os.system(rename_command)
+os.system('python '+where_the_scripts_are+'mesa_to_GR1D.py '+profile+' '+profile+'.short')
 #produce the isotope file
-os.system('python ./MESA_isotopes.py '+profile+' '+profile+'iso.dat')
+os.system('python '+where_the_scripts_are+'MESA_isotopes.py '+profile+' '+profile+'.iso.dat')
